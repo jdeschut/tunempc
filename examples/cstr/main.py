85200e6c2ec160b6892409dd227b2492b037dea3
Jochem De Schutter <jochem.de.schutter@imtek.uni-freiburg.de>
2020-04-06 16:31:29 +0200

    add code generation to CSTR example

--- examples/cstr/main.py
+++ examples/cstr/main.py
@@ -64,7 +64,7 @@ cost, cost_comp = cstr.objective(x,u,data)
 
 # solve steady state problem
 tuner = tunempc.Tuner(
-    f = cstr.dynamics(x, u, data),
+    f = cstr.dynamics(x, u, data)[0],
     l = cost,
     h = h,
     p = 1
@@ -92,6 +92,31 @@ ctrls['tracking'] = tuner.create_mpc('tracking', N, opts=opts, tuning=tuningTn)
 # tuned tracking mpc controller
 ctrls['tuned'] = tuner.create_mpc('tuned', N, opts=opts)
 
+ACADOS_CODEGENERATE = False
+if ACADOS_CODEGENERATE:
+
+    # get system ode
+    ode = ca.Function('ode',[x.cat,u.cat],[cstr.dynamics(x,u,data)[1]['ode']])
+
+    # solver options
+    opts = {}
+    opts['qp_solver'] = 'FULL_CONDENSING_QPOASES' # PARTIAL_CONDENSING_HPIPM
+    opts['hessian_approx'] = 'GAUSS_NEWTON'
+    opts['integrator_type'] = 'ERK'
+    opts['nlp_solver_type'] = 'SQP' # SQP_RTI
+    opts['qp_solver_cond_N'] = 1 # ???
+    opts['print_level'] = 0
+    opts['sim_method_num_steps'] = data['num_steps']
+    opts['tf'] = N*data['ts']
+    opts['nlp_solver_max_iter'] = 300
+    opts['nlp_solver_step_length'] = 1.0
+
+    acados_ocp_solver, acados_integrator = ctrls['tuned'].generate(
+        ode, opts = opts, name = 'unicycle'
+        )
+
+    ctrls_acados = {'tuned_acados': ctrls['tuned']}
+
 # check equivalence
 alpha = np.linspace(-0.1, 1.0, 2)
 x0 = wsol['x',0]
@@ -99,41 +124,60 @@ dx_diehl = np.array([1.0,0.5, 100.0, 100.0]) - wsol['x',0]
 
 # compute open-loop prediction for list of deviations in different state directions
 log = []
+log_acados = []
 for dist in [0]:
     dx = np.zeros((nx,1))
     dx[dist] = dx_diehl[dist]
     log.append(clt.check_equivalence(ctrls, cost, tuner.sys['h'], x0, dx, alpha))
 
+    if ACADOS_CODEGENERATE:
+        log_acados.append(clt.check_equivalence(ctrls_acados, cost, tuner.sys['h'], x0, dx, alpha, flag='acados'))
+
 fig_num = 1
 alpha_plot = -1 # alpha value of interest
 dx_plot = 0 # state direction of interest
-for name in list(ctrls.keys()):
+ctrl_list = list(ctrls.keys())
+if ACADOS_CODEGENERATE:
+    ctrl_list += list(ctrls_acados.keys())
+for name in ctrl_list:
+
+    if name[-6:] != 'acados':
+        logg = log[dx_plot][alpha_plot]
+        ls = '-'
+    else:
+        logg = log_acados[dx_plot][alpha_plot]
+        ls = '--'
 
     # plot controls
     plt.figure(fig_num)
     for i in range(nu):
         plt.subplot(nu,1,i+1)
-        plt.step(tgrid, [log[dx_plot][alpha_plot]['u'][name][j][i] for j in range(len(tgrid))], where='post')
+        plt.step(tgrid, [logg['u'][name][j][i] for j in range(len(tgrid))], where='post', linestyle = ls)
         plt.grid(True)
         plt.autoscale(enable=True, axis='x', tight=True)
 
     # plot stage cost deviation
     plt.figure(fig_num +1)
-    stage_cost_dev = [x[0] - x[1] for x in zip(log[dx_plot][alpha_plot]['l'][name],N*[lOpt])]
-    plt.step(tgrid, stage_cost_dev, where='post')
+    stage_cost_dev = [x[0] - x[1] for x in zip(logg['l'][name],N*[lOpt])]
+    plt.step(tgrid, stage_cost_dev, where='post', linestyle = ls)
 
     # plot state prediction
     plt.figure(fig_num +2)
     for i in range(nx):
         plt.subplot(nx,1,i+1)
-        plt.plot(tgridx, [log[dx_plot][alpha_plot]['x'][name][j][i] for j in range(len(tgridx))])
-        plt.plot(tgridx, [wsol['x',0][i] for j in range(len(tgridx))],  linestyle='--', color='black')
+        plt.plot(tgridx, [logg['x'][name][j][i] for j in range(len(tgridx))], linestyle = ls)
+        plt.plot(
+            tgridx,
+            [wsol['x',0][i] for j in range(len(tgridx))],
+            linestyle='--',
+            color='black',
+            label='_nolegend_')
         plt.autoscale(enable=True, axis='x', tight=True)
         plt.grid(True)
 
 # adjust control plot
 plt.figure(fig_num)
-plt.legend(list(ctrls.keys()))
+plt.legend(ctrl_list)
 plt.subplot(nu,1,1)
 plt.title('Feedback controls')
 plt.subplot(nu,1,nu)
@@ -141,7 +185,7 @@ plt.xlabel('t [s]')
 
 # adjust stage cost plot
 plt.figure(fig_num+1)
-plt.legend(list(ctrls.keys()))
+plt.legend(ctrl_list)
 plt.grid(True)
 plt.xlabel('t [s]')
 plt.title('Stage cost deviation')
@@ -150,7 +194,7 @@ plt.autoscale(enable=True, axis='x', tight=True)
 # adjust state plot
 plt.figure(fig_num+2)
 plt.subplot(nx,1,1)
-plt.legend(list(ctrls.keys()))
+plt.legend(ctrl_list)
 plt.title('State trajectories')
 plt.subplot(nx,1,nx)
 plt.xlabel('t [s]')
@@ -160,16 +204,24 @@ fig_num += 3
 # plot transient cost vs. alpha
 plt.figure(fig_num)
 transient_cost = {}
-for name in list(ctrls.keys()):
+for name in ctrl_list:
+
+    if name[-6:] != 'acados':
+        logg = log[dx_plot]
+        ls = '-'
+    else:
+        logg = log_acados[dx_plot]
+        ls = '--'
+
     transient_cost[name] = []
     for i in range(len(alpha)):
             transient_cost[name].append(
-                sum([x[0] - x[1] for x in zip(log[dx_plot][i]['l'][name],N*[lOpt])])
+                sum([x[0] - x[1] for x in zip(logg[i]['l'][name],N*[lOpt])])
                 )
-    plt.plot(alpha, transient_cost[name], marker = 'o')
+    plt.plot(alpha, transient_cost[name], marker = 'o', linestyle = ls)
 
 plt.grid(True)
-plt.legend(list(ctrls.keys()))
+plt.legend(ctrl_list)
 plt.title('Transient cost')
 plt.xlabel('alpha [-]')
 
